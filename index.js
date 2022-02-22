29193227319f7abaf13e8b73a66e7244f3264d7b
reng <reng@ultracombos.com>
2022-02-22 12:02:40 +0800

    add test node server

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const http = require('http');
+const fs = require('fs');
+var cors = require('cors');
+var app = express();
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/html' })
+//   fs.createReadStream('auto_index.html').pipe(res)
+// });
+
+// server.listen(process.env.PORT || 3000);
+
+app.use(cors({ origin: 'https://bmxf17dhzg.execute-api.ap-northeast-1.amazonaws.com', credentials :  true }));
+app.use(express.static('.'));
+
+// app.get('*', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
